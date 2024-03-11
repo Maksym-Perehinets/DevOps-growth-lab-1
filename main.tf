43841c773754bdee777a255ce19d3919b99ef005
Perehinets Maksym <supercomf128@gmail.com>
2024-03-11 19:37:12 +0200

    final

--- main.tf
+++ main.tf
@@ -25,16 +25,24 @@ resource "azurerm_subnet" "production" {
   resource_group_name  = azurerm_resource_group.BeStrong-rg.name
   virtual_network_name = azurerm_virtual_network.production.name
   address_prefixes     = ["10.10.1.0/24"]
-  service_endpoints = ["Microsoft.KeyVault"]
+  service_endpoints    = ["Microsoft.KeyVault"]
 
   # To allow conncetion with web app services add delegation to Microsoft.Web/serverFarms
   delegation {
     name = "allow-web-app"
     service_delegation {
       name = "Microsoft.Web/serverFarms"
+      actions = [ "Microsoft.Network/virtualNetworks/subnets/action" ]
     }
   }
+}
 
+resource "azurerm_subnet" "production-db" {
+  name                 = "tf-production-db-subnet"
+  resource_group_name  = azurerm_resource_group.BeStrong-rg.name
+  virtual_network_name = azurerm_virtual_network.production.name
+  address_prefixes     = ["10.10.2.0/24"]
+  private_endpoint_network_policies_enabled = true
 }
 
 # Key Vault creation
@@ -50,26 +58,11 @@ resource "azurerm_key_vault" "main-kv" {
   sku_name = "standard"
 
   network_acls {
-    bypass = "AzureServices"
-    default_action = "Deny"
+    bypass                     = "AzureServices"
+    default_action             = "Deny"
     virtual_network_subnet_ids = [azurerm_subnet.production.id]
   }
 }
-
-# #Default access policy for terraform service
-# resource "azurerm_key_vault_access_policy" "terrafrom-access-policy" {
-#   key_vault_id = azurerm_key_vault.main-kv.id
-#   tenant_id    = data.azurerm_client_config.current.tenant_id
-#   object_id    = data.azurerm_client_config.current.object_id
-
-#   lifecycle {
-#     create_before_destroy = true
-#   }
-
-#   key_permissions         = var.kv-key-permissions-full
-#   secret_permissions      = var.kv-secret-permissions-full
-# }
-
 
 # Application Insights app declaretion
 resource "azurerm_application_insights" "main-app-ins" {
@@ -79,39 +72,29 @@ resource "azurerm_application_insights" "main-app-ins" {
   application_type    = "web"
 }
 
-# resource "azurerm_key_vault_secret" "AppInsights-InstKey-secret" {
-#   # Secret for APPINSIGHTS_INSTRUMENTATIONKEY
-
-#   name         = "tf-AppInsights-InstrumentationKey-${random_id.tf-RandomPrefix.hex}"
-#   value        = azurerm_application_insights.main-app-ins.instrumentation_key
-#   key_vault_id = azurerm_key_vault.main-kv.id
-  
-#   lifecycle {
-#     ignore_changes = [value, version]
-#   }
-# }
-
-# resource "azurerm_key_vault_secret" "ApplicationInsights-Connection-secret" {
-#   # Secret for APPLICATIONINSIGHTS_CONNECTION_STRING
-
-#   name         = "tf-ApplicationInsights-Connection-String-${random_id.tf-RandomPrefix.hex}"
-#   value        = azurerm_application_insights.main-app-ins.connection_string
-#   key_vault_id = azurerm_key_vault.main-kv.id
-
-#   lifecycle {
-#     ignore_changes = [value, version]
-#   }
-# }
-
 # Storage account creation with endpoint and file share
 resource "azurerm_storage_account" "file-share" {
-  name                     = "storageaccountname"
+  name                     = "tffileshare${random_id.tf-RandomPrefix.hex}"
   resource_group_name      = azurerm_resource_group.BeStrong-rg.name
   location                 = azurerm_resource_group.BeStrong-rg.location
   account_tier             = "Standard"
   account_replication_type = "GRS"
 }
 
+# Private endpoint configuration to default subnet for storage account
+resource "azurerm_private_endpoint" "StorageAccount-PrivateEndpoint" {
+  name                = "tf-StorageAccount-endpoint"
+  resource_group_name = azurerm_resource_group.BeStrong-rg.name
+  location            = azurerm_resource_group.BeStrong-rg.location
+  subnet_id           = azurerm_subnet.production-db.id
+
+  private_service_connection {
+    name                           = "tf-StorageAccount-PrivateServiceConnection"
+    private_connection_resource_id = azurerm_storage_account.file-share.id
+    subresource_names              = ["blob"]
+    is_manual_connection           = false
+  }
+}
 
 # Service plan with service app declaretion. With additional key vault access policy set up
 resource "azurerm_service_plan" "default" {
@@ -132,9 +115,18 @@ resource "azurerm_linux_web_app" "web-app" {
   identity {
     type = "SystemAssigned"
   }
+  storage_account {
+    access_key   = azurerm_storage_account.file-share.primary_access_key
+    account_name = azurerm_storage_account.file-share.name
+    name         = "fileshare"
+    share_name   = "fileshare"
+    type         = "AzureFiles"
+    mount_path   = "/file-share"
+  }
+
   app_settings = {
-    APPINSIGHTS_INSTRUMENTATIONKEY        = azurerm_application_insights.main-app-ins.instrumentation_key
-    APPLICATIONINSIGHTS_CONNECTION_STRING = azurerm_application_insights.main-app-ins.connection_string
+    APPINSIGHTS_INSTRUMENTATIONKEY             = azurerm_application_insights.main-app-ins.instrumentation_key
+    APPLICATIONINSIGHTS_CONNECTION_STRING      = azurerm_application_insights.main-app-ins.connection_string
     ApplicationInsightsAgent_EXTENSION_VERSION = "~3"
   }
 }
@@ -142,7 +134,7 @@ resource "azurerm_linux_web_app" "web-app" {
 # Garant App service identety access to Key Vault
 resource "azurerm_key_vault_access_policy" "AppService-conncetion" {
   key_vault_id = azurerm_key_vault.main-kv.id
-  tenant_id = data.azurerm_client_config.current.tenant_id
+  tenant_id    = data.azurerm_client_config.current.tenant_id
   object_id    = azurerm_linux_web_app.web-app.identity[0].principal_id
   key_permissions = [
     "Get",
@@ -164,35 +156,40 @@ resource "azurerm_container_registry" "acr-default" {
 
 # Assign roles to app service to be able to pull and push images from ACR
 resource "azurerm_role_assignment" "app-service-PullPush" {
-  scope = azurerm_container_registry.acr-default.id
+  scope                = azurerm_container_registry.acr-default.id
   role_definition_name = "AcrPush"
-  principal_id = azurerm_linux_web_app.web-app.identity[0].principal_id
+  principal_id         = azurerm_linux_web_app.web-app.identity[0].principal_id
 }
 
 # MS sql db creation
-resource "azurerm_mssql_server" "mssql-db" {
-  name                         = "tf-MsSql-db-${random_id.tf-RandomPrefix.hex}"
-  resource_group_name          = azurerm_resource_group.BeStrong-rg.name
-  location                     = azurerm_resource_group.BeStrong-rg.location
-  version                      = "12.0"
+  # Server creation
+resource "azurerm_mssql_server" "mssql-server" {
+  name                = "tf-mssql-server-${random_id.tf-RandomPrefix.hex}"
+  resource_group_name = azurerm_resource_group.BeStrong-rg.name
+  location            = azurerm_resource_group.BeStrong-rg.location
+  version             = "12.0"
 
   administrator_login          = var.MSSQL-AdministratorLogin
   administrator_login_password = var.MSSQL-AdministratorPassword
 }
+  # DB creation
+resource "azurerm_mssql_database" "db" {
+  name      = "tf-mssql-db"
+  server_id = azurerm_mssql_server.mssql-server.id
+}
 
 
-# Private endpoint configuration to default subnet
-resource "azurerm_private_endpoint" "mssql-db-PrivateEndpoint" {
-  name                = "tf-MsSql-db-endpoint-${random_id.tf-RandomPrefix.hex}"
+# Private endpoint configuration to default database designated subnet for ms sql
+resource "azurerm_private_endpoint" "mssql-server-PrivateEndpoint" {
+  name                = "tf-MsSql-db-endpoint"
   resource_group_name = azurerm_resource_group.BeStrong-rg.name
   location            = azurerm_resource_group.BeStrong-rg.location
-  subnet_id           = azurerm_subnet.production.id
+  subnet_id           = azurerm_subnet.production-db.id
 
   private_service_connection {
-    name                           = "${random_id.tf-RandomPrefix.hex}-privateserviceconnection"
-    private_connection_resource_id = azurerm_mssql_server.mssql-db.id
-    subresource_names              = [azurerm_subnet.production.name]
+    name                           = "tf-MsSql-PrivateServiceConnection"
+    private_connection_resource_id = azurerm_mssql_server.mssql-server.id
+    subresource_names              = ["sqlServer"]
     is_manual_connection           = false
   }
 }
-
