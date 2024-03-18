12257679f207fe35448838dbd19828bff9bc3c29
Perehinets Maksym <supercomf128@gmail.com>
2024-03-18 15:20:03 +0200

    test3

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@
 locals {
   location = replace(lower(var.location), " ", "-")
 }
-# Client info test pip
+# Client info test pi
 data "azurerm_client_config" "current" {}
 
 # Resource group creation
